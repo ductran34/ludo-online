3030fecdb48a57fd2903e1dc4e6908b6cb50e277
ductran34 <45677980+ductran34@users.noreply.github.com>
2026-02-12 21:54:43 +0000

    Move Next.js project to root

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+}
+
+module.exports = nextConfig
