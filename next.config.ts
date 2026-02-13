03ad1537fab3be0b66276d4ec0bc9c3fd3f53a99
ductran34 <45677980+ductran34@users.noreply.github.com>
2026-02-13 03:46:53 +0000

    npm install

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
 }
 
 module.exports = nextConfig
